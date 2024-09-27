c043618c6a3ef23f07530ef59767a17c901bfd71
palak-sgl <palak@shipglobal.in>
2024-09-27 17:55:41 +0530

    commiting first

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./main/*.html"],
+  theme: {
+    extend: {
+      spacing:{
+        53:"112px"      }
+    },
+  },
+  plugins: [],
+}
+
